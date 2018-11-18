da6a0d0fa710562044b58a78e7df8e2768fb1fb8
amitabhleo <amitabhleo@gmail.com>
2018-11-18 13:24:29 +0530

    first javascript fb project

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,34 @@
-4 + 3;
+//my first facebook app
 
-if (4 + 3 === 7){
-
-console.log("you are smart !");
+//defining the user name password in a
+var dataBase = [
+  {
+  userName: "amitabh",
+  passWord: "supersecret"
 }
+];
+//defining a newsFeed
+var newsFeed =[
+  {
+    userName: "Sally",
+    timeLine: "It is a very tiring day today!"
+  },
+  {
+    userName: "betty",
+  timeLine: "javaScript is so cool !"
+}
+];
+//defining the function
+var userNamePrompt = prompt("what is your username");
+var passwordPrompt = prompt("what is your passWord");
+//comparing the username password and then showin the newsFeed
+function signIn(user,passwd){
+  if(user === dataBase[0].userName &&
+  passwd === dataBase[0].passWord){
+    console.log(newsFeed);
+  }
+  else {
+    console.log("username or password not right");
+  }
+}
+signIn(userNamePrompt,passwordPrompt);
