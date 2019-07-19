fc54975e62d623a585ee1387bf3fcad41aa3a320
amitabhleo <amitabhleo@gmail.com>
2019-07-19 08:48:22 +0530

    creating myFacebookApp

---
+++ JavaScript/myFacebookApp/script.js
@@ -0,0 +1,21 @@
+//creating a database object
+var database = [
+    {
+        username: "amitabh",
+        password: "secret"
+    }
+];
+//creating a newsfeed object
+var newsfeed = [
+    {
+        username: "andrie",
+        timeline: "Ah it is a wonderful day!"
+    },
+    {
+        username: "sally",
+        timeline: "I love javaScript"
+    }
+];
+//Using prompt to ask for the username
+var promptUserName = prompt("what is your username ?");
+var promptPassword = prompt("what is your password ?");
