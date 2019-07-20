5921c08d242efe1b8add3ad3d240032e5664226c
amitabhleo <amitabhleo@gmail.com>
2019-07-20 08:06:31 +0530

    multiple user facebook

--- JavaScript/myFacebookApp/script.js
+++ JavaScript/myFacebookApp/script.js
@@ -3,6 +3,14 @@ var database = [
     {
         username: "amitabh",
         password: "secret"
+    },
+    {
+        username: "geetanjali",
+        password: "123"
+    },
+    {
+        username: "megha",
+        password: "321"
     }
 ];
 //creating a newsfeed object
@@ -19,3 +27,25 @@ var newsfeed = [
 //Using prompt to ask for the username
 var promptUserName = prompt("what is your username ?");
 var promptPassword = prompt("what is your password ?");
+// do not repeat yourself checking if user is valid
+function isUserValid(user,pass){
+    //database.forEach(i){ need to check the syntax
+    for (i=0;i<database.length;i++){
+        if (database[i].username === user
+            && database[i].password === pass){
+               return true;
+            }
+        }
+        return false;
+}
+//passing the prompt to function
+function userSignIn (username,password){
+    //checking if user is valid 
+    //console.log(isUserValid(username,password));
+    if (isUserValid(username,password)){
+        console.log(newsfeed);
+    }
+     else alert ("wrong username or password !");   
+}
+
+userSignIn(promptUserName,promptPassword);
