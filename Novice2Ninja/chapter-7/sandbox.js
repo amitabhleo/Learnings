3348bfdab745e14815196e0b180a4cbf5a7b5d47
amitabhleo <amitabhleo@gmail.com>
2019-08-14 22:32:08 +0530

    chapter-7 #1 contd.

--- Novice2Ninja/chapter-7/sandbox.js
+++ Novice2Ninja/chapter-7/sandbox.js
@@ -1,1 +1,32 @@
-console.log('chapter-7')
+
+  // pulled up so that have access to all adding a regex (regular expression to test validation)
+  const usernamePattern = /^[a-zA-Z0-9]{6,12}$/;
+
+const form = document.querySelector('.signup-form');
+
+const feedback = document.querySelector('.feedback');
+//const usrname = document.querySelector('#username');
+
+form.addEventListener('submit',e =>{
+    e.preventDefault();
+    //console.log(usrname.value);
+    //console.log(form.username.value);
+    const username = form.username.value;
+  
+    
+    if (usernamePattern.test(username)){
+        feedback.textContent = 'this is a valid username';
+    }else
+    feedback.textContent = 'username should be 6 to 12 char long with no special char.'
+});
+//live form
+
+form.username.addEventListener('keyup',e =>{
+    //console.log(e.target.value, form.username.value);
+    if(usernamePattern.test(e.target.value)){
+       // console.log('success');
+       form.username.setAttribute('class','success');
+    }else
+    //console.log('failure');
+    form.username.setAttribute('class','error');
+});
