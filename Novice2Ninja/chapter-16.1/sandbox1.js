0c0269c91bc4d0cdbdaf7ff381d8ee60dcbf07f3
amitabhleo <amitabhleo@gmail.com>
2019-09-22 17:05:34 +0530

    updating native

--- Novice2Ninja/chapter-16.1/sandbox1.js
+++ Novice2Ninja/chapter-16.1/sandbox1.js
@@ -63,53 +63,55 @@ fileButton.addEventListener("change", e => {
   // Points to the images root reference
   var storageRef = firebase.storage().ref("images/" + file.name);
 
-  //let task = storageRef.put(file)
-  // storageRef
-  //   .put(file)
-  //   .then(snapshot => {
-  //     console.log(snapshot.ref.getDownloadURL());
-  //     changeImage(snapshot.ref.getDownloadURL());
-  //     console.log(Promise.resolve());
-  //     //addSts(s,'http:\\google.com');
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
-
-  var uploadTask = storageRef.put(file);
-
-  // Register three observers:
-  // 1. 'state_changed' observer, called any time the state changes
-  // 2. Error observer, called on failure
-  // 3. Completion observer, called on successful completion
-  uploadTask.on(
-    "state_changed",
-    function(snapshot) {
-      // Observe state change events such as progress, pause, and resume
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log("Upload is " + progress + "% done");
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log("Upload is paused");
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log("Upload is running");
-          break;
-      }
-    },
-    function(error) {
-      // Handle unsuccessful uploads
-    },
-    function() {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+  //let task = storageRef.put(file);
+  storageRef
+    .put(file)
+    .then(snapshot => {
+      snapshot.ref.getDownloadURL().then(function(downloadURL) {
         console.log("File available at", downloadURL);
         changeImage(downloadURL);
         const tempUrl = downloadURL;
         addSts('new file from fireStore',tempUrl);
       });
-    }
-  );
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
+//   var uploadTask = storageRef.put(file);
+
+//   // Register three observers:
+//   // 1. 'state_changed' observer, called any time the state changes
+//   // 2. Error observer, called on failure
+//   // 3. Completion observer, called on successful completion
+//   uploadTask.on(
+//     "state_changed",
+//     function(snapshot) {
+//       // Observe state change events such as progress, pause, and resume
+//       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+//       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//       console.log("Upload is " + progress + "% done");
+//       switch (snapshot.state) {
+//         case firebase.storage.TaskState.PAUSED: // or 'paused'
+//           console.log("Upload is paused");
+//           break;
+//         case firebase.storage.TaskState.RUNNING: // or 'running'
+//           console.log("Upload is running");
+//           break;
+//       }
+//     },
+//     function(error) {
+//       // Handle unsuccessful uploads
+//     },
+//     function() {
+//       // Handle successful uploads on complete
+//       // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+//       uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+//         console.log("File available at", downloadURL);
+//         changeImage(downloadURL);
+//         const tempUrl = downloadURL;
+//         addSts('new file from fireStore',tempUrl);
+//       });
+//     }
+//   );
+// });
