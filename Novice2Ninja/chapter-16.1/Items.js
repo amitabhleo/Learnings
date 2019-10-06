eaba0177bf450cf77eb1c1c13b0afddd26209ace
amitabhleo <amitabhleo@gmail.com>
2019-10-06 19:59:06 +0530

    adding sub collection hard coded

--- Novice2Ninja/chapter-16.1/Items.js
+++ Novice2Ninja/chapter-16.1/Items.js
@@ -1,37 +1,51 @@
-const cafeList = document.querySelector('#cafe-list');
-const form = document.querySelector("#add-cafe-form");
+const cafeList = document.querySelector('#items-list');
+const form = document.querySelector("#add-items-form");
+const header = document.querySelector("h2");
+
+//TODO:updating cafe header
+//header.innerHTML = `<h2>Cafe Name Here</h2>`;
 
 //creating a function to render cafe
 function renderCafe(doc){
     //creating elements to li
     let li = document.createElement("li");
     let name = document.createElement("span");
-    let city = document.createElement("span");
+    //let city = document.createElement("span");
     let item = document.createElement("span");
     let image = document.createElement("img");
-    let btn = document.createElement("BUTTON")
+    //let btn = document.createElement("BUTTON")
     //adding elements to li
     li.setAttribute("data-id",doc.id);
     name.textContent = doc.data().name;
     item.textContent = doc.data().item;
-    city.textContent = doc.data().city;
-    image.src = doc.data().photo;
-    btn.innerHTML = `<input type="file" name="fileButton" id="fileButton" accept="image/*" ></input>`;
+    // city.textContent = doc.data().city;
+    image.src = doc.data().item_photo;
+    //btn.innerHTML = `<input type="file" name="fileButton" id="fileButton" accept="image/*" ></input>`;
 
     li.appendChild(name);
     li.appendChild(item);
-    li.appendChild(city);
+    // li.appendChild(city);
     li.appendChild(image);
 
     cafeList.appendChild(li);
 }
-//querying Firebase and getting the data
-db.collection('vendors').orderBy('name')
-.get().then(snapshot => {
-       snapshot.docs.forEach(doc => {
-        renderCafe(doc);
+//old querying Firebase and getting the data
+    // db.collection('vendors').orderBy('city')
+    // .get().then(snapshot => {
+    //     snapshot.docs.forEach(doc => {
+    //         renderCafe(doc);
+    //     });
+    // });
+//another way to query sub-collection this seems a better way -working code
+    db.collection('vendors').doc('QNk3fg7fBgiSVjAAc9H4').collection('items')
+    .get().then(snapshot => {
+       
+          snapshot.docs.forEach(doc => {
+            //console.log(doc.data().item_photo);
+            renderCafe(doc);
+        });
     });
-});
+// saving data
 
 //Old codeadding new restaurant to firebase
     // form.addEventListener("submit", e => {
@@ -46,13 +60,14 @@ db.collection('vendors').orderBy('name')
     // });
 
 //TODO:adding a new restaurant from a function after the image is uploaded
-    const addCafe = (url =>{
+    const addItem = (url =>{
             console.log("photo link is : ",url);
-            db.collection("vendors").add({
-            name: form.name.value,
-            city: form.city.value,
-            photo: url
-            
+            db.collection("vendors").doc('QNk3fg7fBgiSVjAAc9H4').collection('items')
+            .add({
+                name: form.name.value,
+                item: form.city.value,
+                item_photo: url,
+                   
             });
             form.name.value = "";
             form.city.value = "";
@@ -78,7 +93,7 @@ fileButton.addEventListener("change", e => {
           var url = "google.com";
             var resizeUrl = downloadURL.replace(".jpg","_100x100.jpg");
             console.log(resizeUrl);
-            addCafe(resizeUrl);
+            addItem(resizeUrl);
           });
       })
       .catch(err => {
