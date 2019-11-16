86e3678bb665be2b7b5b7c57142482f40905f19e
amitabhleo <amitabhleo@gmail.com>
2019-11-16 11:18:40 +0530

    added config file

---
+++ Novice2Ninja/chapter-16.1/config.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDt3EFayh17qgDing_Nas1hjJG86nk8SDQ",
+    authDomain: "my-family-9ae9d.firebaseapp.com",
+    databaseURL: "https://my-family-9ae9d.firebaseio.com",
+    projectId: "my-family-9ae9d",
+    storageBucket: "my-family-9ae9d.appspot.com",
+    messagingSenderId: "643234801405",
+    appId: "1:643234801405:web:b775d96ae67952bc",
+    measurementId: "G-FBDET9F692"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  //firebase.analytics();
