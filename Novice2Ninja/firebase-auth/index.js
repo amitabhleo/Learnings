43783402cdf708763ba8511090d0022e7b1f7c02
amitabhleo <amitabhleo@gmail.com>
2019-11-19 11:00:09 +0530

    firebase-auth

---
+++ Novice2Ninja/firebase-auth/index.js
@@ -0,0 +1,47 @@
+//getting user email and password
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+     document.getElementById("user_div").style.display = "block";
+     document.getElementById("login_div").style.display = "none";
+    } else {
+      // No user is signed in.
+      document.getElementById("user_div").style.display = "none";
+     document.getElementById("login_div").style.display = "block";
+    }
+  });
+
+let login = document.querySelector("#login");
+//TODO: add event lister is a call back function
+login.addEventListener("click", evnt =>{
+    let useremail = document.getElementById("email").value;
+    let userpassword = document.getElementById("password").value;
+    // alert("login clicked");
+    // console.log(useremail);
+    // console.log(userpassword);
+    //TODO:copied from firebase
+    firebase.auth().createUserWithEmailAndPassword(useremail, userpassword)
+    .then(snapshot =>{
+        console.log(snapshot.user.email);
+        console.log(snapshot.user.uid);
+     
+    })
+    .catch(function(error) {
+        // Handle Errors here.
+         var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert("error msg :"+ error.message);
+        // ...
+      });
+
+});
+let logout = document.querySelector("#logout");
+logout.addEventListener("click",evnt =>{
+    //TODO:paste from firebase
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
+})
